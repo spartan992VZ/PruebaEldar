43f3fa27dfda6b48b37b66ec57b9f20cc0474558
Jaime Santiago <jaimesantiagojrr@gmail.com>
2024-05-08 08:06:39 -0300

    Primer commit

---
+++ src/main/java/com/eldar/prueba/TDC/Operations/validations/TdcOperationsValidationsApplication.java
@@ -0,0 +1,13 @@
+package com.eldar.prueba.TDC.Operations.validations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdcOperationsValidationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdcOperationsValidationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eldar/prueba/TDC/Operations/validations/TdcOperationsValidationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eldar.prueba.TDC.Operations.validations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdcOperationsValidationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
